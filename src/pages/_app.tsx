f0926d4c8ea749daa9fffa8ee3e8cee2fc9f2a38
Gabriel Pires <gabpiresdev@gmail.com>
2022-05-18 21:03:01 -0300

    chore: chakra ui setup and theme start props

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../styles/theme/theme';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Head>
+        <title>KeepDice</title>
+      </Head>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { Flex, Heading, Text } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <h2>KeepDice</h2>;
+  return (
+    <Flex
+      h={'100vh'}
+      flexDir={'column'}
+      alignItems={'center'}
+      justifyContent={'center'}
+    >
+      <Heading>KeepDice</Heading>
+      <Text>Coming soon...</Text>
+    </Flex>
+  );
 };
 
 export default Home;
